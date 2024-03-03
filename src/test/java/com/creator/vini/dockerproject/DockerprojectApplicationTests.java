b3bcc4b262b3f2f90d63537ec411ab5e2ce2082f
vinicinrn <zviniciusnascimento@gmail.com>
2024-03-03 16:26:47 -0300

    first commit

---
+++ src/test/java/com/creator/vini/dockerproject/DockerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creator.vini.dockerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
